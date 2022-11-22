cf2908de3eda5a3b870df8b5fb1cccfeed3dfdbc
Douglas <dfjuarezreyes21@gmail.com>
2022-11-22 15:00:37 -0600

    validation in createUser route added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -17,7 +17,14 @@ router.get("/", getUserById);
 
 router.post(
   "/",
-  [check("email", "The email is not valid").isEmail()],
+  [
+    check("email", "The email is not valid").isEmail(),
+    check("password", "The password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+    check("name", "The name is required").not().isEmpty(),
+    check("role", "It is not a valid role").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+  ],
   createUser
 );
 
