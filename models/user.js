df080eaae0e7da5bf034fe4df5f87403482af5ef
Douglas <dfjuarezreyes21@gmail.com>
2022-11-22 11:07:33 -0600

    create user model for mongodb

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: { type: String, require: [true, "The name is required"] },
+  email: {
+    type: String,
+    require: [true, "The email is required"],
+    unique: true,
+  },
+  password: { type: String, require: [true, "The password is require"] },
+  img: { type: String },
+  role: { type: String, require: true, enum: ["ADMIN_ROLE", "USER_ROLE"] },
+  status: { type: Boolean, default: true },
+  google: { type: Boolean, default: false },
+});
+
+module.exports = model("User", UserSchema);
