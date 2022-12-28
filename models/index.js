0a797d88adaf8b08624604a5372bf5fda5f5498d
Douglas <dfjuarezreyes21@gmail.com>
2022-12-28 10:44:01 -0600

    controller index added

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Categories = require("./category");
+const Role = require("./role");
+const User = require("./user");
+const Server = require("./server");
+
+module.exports = {
+  Categories,
+  Role,
+  User,
+  Server,
+};
