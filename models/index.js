44303459b856739de39eee3e9d893a36f574b579
Douglas <dfjuarezreyes21@gmail.com>
2022-12-29 12:28:23 -0600

    product index added

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
+const Product = require("./product");
 const Categories = require("./category");
 const Role = require("./role");
 const User = require("./user");
 const Server = require("./server");
 
 module.exports = {
+  Product,
   Categories,
   Role,
   User,
